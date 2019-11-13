85f5316c00b382b67417c929f538c3d158b247ad
maitreebain <maitreebain@gmail.com>
2019-11-13 12:14:37 -0500

    set up all outlets working on readline and textfield functions

---
+++ Hangman/Hangman/HangmanUsersIntake.swift
@@ -0,0 +1,26 @@
+//
+//  HangmanUsersIntake.swift
+//  Hangman
+//
+//  Created by Maitree Bain on 11/13/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+enum AnswerStatus {
+    case correct
+    case incorrect
+}
+
+class readLineIntake {
+    
+    var guessingNum = 0
+    
+//    func userIntake(userAnswerStatus: Character) -> AnswerStatus {
+//
+//
+//        return AnswerStatus // correct / incorrect
+//    }
+    
+}
--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var randomWordLabel: UILabel!
+    
+    @IBOutlet weak var playerOneTextField: UITextField!
+    
+    @IBOutlet weak var playerTwoTextField: UITextField!
+    
+    var alphabet: [String] = ["abcdefghijklmnopqrstuvwxyz"]
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    playerOneTextField.delegate = self
+    playerTwoTextField.delegate = self
   }
-
+    
 
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print("Ran into should begin editing")
+        return true
+    }
+    
+
+    
+    
+}
